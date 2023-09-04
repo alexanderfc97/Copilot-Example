b1dcaee9be0d9cb007dd420ba580e986002399a8
alexfc97 <alexfc@live.dk>
2023-09-04 15:57:28 +0200

    .

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        addNumbers();
+        }
+
+    private static void addNumbers() {
+        // take the numbers 5 and 10 and add them together
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
---
+++ src/test/java/IsPrimeTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class IsPrimeTest {
+
+    // Math.isPrime(int) returns whether the given number is prime or not
+    @Test
+    public void isPrimeTest(){
+    }
+
+
+}
